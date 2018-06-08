080af516f3002ce98e604b16352a4ad298b95a6e
jcab118 <32029661+jcab118@users.noreply.github.com>
2018-06-07 20:00:49 -0400

    sec push

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,45 +1,65 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom'
 import Header from './Header';
 
-export default class Home extends React.Component {
+class Sign_up extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+    }
+  }
+  signUpForm(e){
+    e.preventDefault();
+      var newUser = {
+        name: this.refs.name.value,
+        email: this.refs.email.value,
+        phone: this.refs.phone.value,
+      }
+      fetch('/api/sign-up', {
+          method: 'post',
+          body: JSON.stringify(newUser),
+          headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json'
+          },
+          credentials: 'same-origin'
+      }).then((response) => response.json())
+      .then((results) => {
+        this.props.history.push("/log_in");
+      }); 
+  }
   render() {
     return (
-      <div>
-        <Header/>
-	  		<div className ="text-center">
-	  			<img id= "code" src = "./images/Logo.jpg"></img>
-	  		</div>
-	  			<div id = "info" className ="text-center"> 
-	  				<p>ODFitness is a fitness bootcamp created by (O)tniel & (D)erek 
-	  				that combines strength and conditioning with cardiovascular endurance to
-	  				create a well-rounded workout.
-	  				</p>
-	  				<p>If you're looking to lose fat, build strength, and improve your 
-	  				cardiovascular endurance; then look no further than ODFitness
-	  				</p>
-	  			</div>	
-	  			<div id = "trainers" className ="text-center">
-		  			<h3>(O)tniel & (D)erek</h3>
-		  			<h4>aka ODFitness</h4>
-	  				<img id= "pic4" src = "./images/IMG_0062.jpeg"></img>
-	  			</div>
-	  			<div id = "pics" className ="text-center">
-	  				<img id= "pic1" src = "./images/IMG_0057.jpeg"></img>
-	  				<img id= "pic2" src = "./images/IMG_0059.jpeg"></img>
-	  				<img id= "pic3" src = "./images/IMG_0060.jpeg"></img>
-	  				<img id= "pic5" src = "./images/IMG_0063.jpeg"></img>
-	  				<img id= "pic6" src = "./images/IMG_0064.jpeg"></img>
-	  				<img id= "pic7" src = "./images/IMG_0065.jpeg"></img>
-	  				<img id= "pic8" src = "./images/IMG_0066.jpeg"></img>
-	  				<img id= "pic9" src = "./images/IMG_0077.jpeg"></img>
-	  				<img id= "pic10" src = "./images/IMG_0078.jpeg"></img>
-	  				<img id= "pic11" src = "./images/IMG_0080.jpeg"></img>
-	  				<img id= "pic12" src = "./images/IMG_0081.jpeg"></img>
-	  			</div>
-	  		</div>
+       <div className="text-center">
+         <Header/>
+         	<img className ="text-center" id= "code" src = "./images/Logo.jpg"></img>
+			<div>
+				<div id ="form">
+					<form id="sign-in-form" onSubmit={this.signUpForm.bind(this)}>
+						<h4>Claim your Free Trial Class!!!!!</h4>
+			            <label>Name</label><br></br>
+			            <input className="text-center" type="text" ref="name" /><br></br>
+			            <label>Email</label><br></br>
+			            <input className="text-center" type="text" ref="email" /><br></br>
+			            <label>Phone</label><br></br>
+			            <input className="text-center" type="text" ref="Phone"/><br></br>
+			            <input className="btn btn-danger" type="submit" />        
+		          	</form>
+		        </div>
+		        <div id = "desc">
+			        <p>ODFitness is a fitness bootcamp created by (O)tniel & (D)erek 
+					that combines strength and conditioning with cardiovascular endurance to
+					create a well-rounded workout.
+					</p>
+					<p>If you're looking to lose fat, build strength, and improve your 
+					cardiovascular endurance; then look no further than ODFitness
+					</p>
+		        </div>
+          	</div>
+       </div>
+    );
+  }
+};
 
-    	)
-  	}
-}
+export default withRouter(Sign_up)
